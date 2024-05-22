f85258eb0358ea8d5ede03ce4411c053433cf026
Rohan <113842997+Rohan00kumar@users.noreply.github.com>
2024-05-22 09:53:50 +0530

    Update GeneralMode.jsx

--- codemate/src/pages/GeneralMode.jsx
+++ codemate/src/pages/GeneralMode.jsx
@@ -1,26 +1,25 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useTheme } from './ThemeContext';
-import Login from './Login';
-import Appss from '../../../Ai Bot/src/Appss';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useTheme } from "./ThemeContext";
+import Login from "./Login";
+import Appss from "../../../Ai Bot/src/Appss";
 export default function GeneralMode() {
-  const [currentMode, setCurrentMode] = useState('General Mode');
+  const [currentMode, setCurrentMode] = useState("General Mode");
   const [isLoginVisible, setIsLoginVisible] = useState(false); // State to manage login form visibility
   const [isMenuOpen, setIsMenuOpen] = useState(false); // State to manage menu visibility
   const navigate = useNavigate();
   const { theme, toggleTheme } = useTheme();
 
   const handleModeChange = (mode) => {
-    if (mode === 'Team Mode') {
-      setIsLoginVisible(true); // Show login form when Team Mode is clicked
-    } else {
+    if (mode === "Team Mode") {
+       navigate("/new-message");
       setCurrentMode(mode);
-      if (mode === 'Individual Mode') {
-        navigate('/individual-mode');
-      } else {
-        navigate('/');
-      }
     }
+    else if (mode === "Individual Mode") {
+      navigate("/indi-message");
+    } else {
+        navigate("/");
+      }
   };
 
   const handleNewChat = () => {
@@ -29,10 +28,7 @@ export default function GeneralMode() {
 
   const handleLogOut = () => {
     // Implement logout logic here
-  };
-
-  const handleBackToDiscord = () => {
-    // Implement back to Discord logic here
+    
   };
 
   const toggleLogin = () => {
@@ -50,9 +46,18 @@ export default function GeneralMode() {
   };
 
   return (
-    <div className={`flex flex-col lg:flex-row w-full h-full min-h-screen box-sizing-border ${theme === 'light' ? 'bg-[#FAF1E4]' : 'bg-black text-white'}`}>
-      {isLoginVisible && <Login onLogin={handleLogin} />} {/* Render Login component if login form is visible */}
-      <div className={`flex flex-col p-6 w-full lg:w-[330px] box-sizing-border ${theme === 'light' ? 'bg-[#8579B2]' : 'bg-[#333]'}`}>
+    <div
+      className={`flex flex-col lg:flex-row w-full h-full min-h-screen box-sizing-border ${
+        theme === "light" ? "bg-[#FAF1E4]" : "bg-black text-white"
+      }`}
+    >
+      {isLoginVisible && <Login onLogin={handleLogin} />}{" "}
+      {/* Render Login component if login form is visible */}
+      <div
+        className={`flex flex-col p-6 w-full lg:w-[330px] box-sizing-border ${
+          theme === "light" ? "bg-[#8579B2]" : "bg-[#333]"
+        }`}
+      >
         <div className="relative m-0 lg:m-0 lg:self-center flex justify-between items-center box-sizing-border">
           <span className="font-['Poppins'] font-bold text-[26px] leading-[1] text-[#FFFFFF]">
             CodeMate
@@ -61,21 +66,25 @@ export default function GeneralMode() {
             <span className="material-icons">menu</span>
           </button>
         </div>
-        <div className={`relative mt-10 mb-10 lg:mt-10 lg:mb-10 w-full h-[1px] box-sizing-border ${isMenuOpen ? 'block' : 'hidden'} lg:block`}>
-          <div className={`w-full h-[1px] ${theme === 'light' ? 'bg-[#414084]' : 'bg-gray-500'}`} />
+        <div
+          className={`relative mt-10 mb-10 lg:mt-10 lg:mb-10 w-full h-[1px] box-sizing-border ${
+            isMenuOpen ? "block" : "hidden"
+          } lg:block`}
+        >
+          <div
+            className={`w-full h-[1px] ${
+              theme === "light" ? "bg-[#414084]" : "bg-gray-500"
+            }`}
+          />
         </div>
-        <div className={`lg:flex lg:flex-col ${isMenuOpen ? 'block' : 'hidden'} lg:block`}>
+        <div
+          className={`lg:flex lg:flex-col ${
+            isMenuOpen ? "block" : "hidden"
+          } lg:block`}
+        >
           <div
             className="rounded-[50px] bg-[#414084] relative mb-4 p-2 w-full text-center cursor-pointer transition duration-300 hover:bg-[#555]"
-            onClick={handleNewChat}
-          >
-            <span className="font-['Poppins'] font-normal text-[15px] text-[#FFFFFF]">
-              New Chat
-            </span>
-          </div>
-          <div
-            className="rounded-[50px] bg-[#414084] relative mb-4 p-2 w-full text-center cursor-pointer transition duration-300 hover:bg-[#555]"
-            onClick={() => navigate('/about')}
+            onClick={() => navigate("/about")}
           >
             <span className="font-['Poppins'] font-normal text-[15px] text-[#FFFFFF]">
               About
@@ -83,18 +92,21 @@ export default function GeneralMode() {
           </div>
           <div
             className="rounded-[50px] bg-[#414084] relative mb-4 p-2 w-full text-center cursor-pointer transition duration-300 hover:bg-[#555]"
-            onClick={() => navigate('/home')}
+            onClick={() => navigate("/home")}
           >
             <span className="font-['Poppins'] font-normal text-[15px] text-[#FFFFFF]">
               Home
             </span>
           </div>
           <div className="flex-grow" />
-          <div
-            className="rounded-[50px] bg-[#49625b] relative mb-4 p-2 w-full text-center cursor-pointer transition duration-300 hover:bg-[#555]"
-           onClick={handleBackToDiscord}
-          >
-            <span className="font-['Poppins'] font-semibold  text-[15px] text-[#FFFFFF]">
+          <div className="rounded-[50px] bg-[#49625b] relative mb-4 p-2 w-full text-center cursor-pointer transition duration-300 hover:bg-[#555]">
+            <span
+              className="font-['Poppins'] font-semibold  text-[15px] text-[#FFFFFF]"
+              onClick={() =>
+                (window.location.href =
+                  "https://discord.com/channels/1113434190541750284/1210132991876276254")
+              }
+            >
               Back to Discord
             </span>
           </div>
@@ -109,30 +121,49 @@ export default function GeneralMode() {
         </div>
       </div>
       <div className="flex flex-col w-full p-1 lg:p-1 box-sizing-border">
-        <div className={`flex flex-row justify-between p-1 w-full mb-1 box-sizing-border rounded-lg ${theme === 'light' ? 'bg-[#414084]' : 'bg-[#444]'}`}>
-          <div className="rounded-[40px] border-[1px_solid_#CEDEBD] flex p- w-full max-w-[495px] box-sizing-border" style={{ borderColor: theme === 'light' ? '#CEDEBD' : '#555' }}>
+        <div
+          className={`flex flex-row justify-between p-1 w-full mb-1 box-sizing-border rounded-lg ${
+            theme === "light" ? "bg-[#414084]" : "bg-[#444]"
+          }`}
+        >
+          <div
+            className="rounded-[40px] border-[1px_solid_#CEDEBD] flex p- w-full max-w-[495px] box-sizing-border"
+            style={{ borderColor: theme === "light" ? "#CEDEBD" : "#555" }}
+          >
             <div className="flex flex-row">
               <div
-                className={`ml-4 p-4 cursor-pointer ${currentMode === 'General Mode' ? 'text-[#601f5d]' : 'text-[#FFFFFF]'}`}
-                onClick={() => handleModeChange('General Mode')}
+                className={`ml-4 p-4 cursor-pointer ${
+                  currentMode === "General Mode"
+                    ? "text-[#f0f0f0]"
+                    : "text-[#FFFFFF]"
+                }`}
+                onClick={() => handleModeChange("General Mode")}
               >
                 <span className="rounded-[50px] bg-[#c578d2] relative mb-4 p-2 w-full text-center cursor-pointer transition duration-300 hover:bg-[#555] font-['Poppins'] font-medium text-[13px]">
                   General Mode
                 </span>
               </div>
               <div
-                className={`ml-4 p-4 cursor-pointer ${currentMode === 'Individual Mode' ? 'text-[#CEDEBD]' : 'text-[#FFFFFF]'}`}
-                onClick={() => handleModeChange('Individual Mode')}
+                className={`ml-4 p-4 cursor-pointer ${
+                  currentMode === "Individual Mode"
+                    ? "text-[#CEDEBD]"
+                    : "text-[#FFFFFF]"
+                }`}
+                onClick={() => handleModeChange("Individual Mode")}
               >
                 <span className=" rounded-[50px] bg-[#c578d2] relative mb-4 p-2 w-full text-center cursor-pointer transition duration-300 hover:bg-[#555] font-['Poppins'] font-medium text-[13px]">
                   Individual Mode
                 </span>
               </div>
               <div
-                className={`ml-4 p-4 cursor-pointer ${currentMode === 'Team Mode' ? 'text-[#CEDEBD]' : 'text-[#FFFFFF]'}`}
-                onClick={() => handleModeChange('Team Mode')}
+                className={`ml-4 p-4 cursor-pointer ${
+                  currentMode === "Team Mode"
+                    ? "text-[#CEDEBD]"
+                    : "text-[#FFFFFF]"
+                }`}
+                onClick={() => handleModeChange("Team Mode")}
               >
-                <span className="rounded-[50px] bg-[#c578d2] relative mb-4 p-2 w-full text-center cursor-pointer transition duration-300 hover:bg-[#555] font-['Poppins'] font-medium text-[13px]">
+                <span className="rounded-[50px]  bg-[#c578d2] relative mb-4 p-2 w-full text-center cursor-pointer transition duration-300 hover:bg-[#555] font-['Poppins'] font-medium text-[13px]">
                   Team Mode
                 </span>
               </div>
@@ -140,24 +171,37 @@ export default function GeneralMode() {
           </div>
           <div className="flex flex-row items-center box-sizing-border">
             <div
-              className="shadow-[0px_0px_14px_0px_rgba(0,0,0,0.15)] rounded-[20px] bg-[#FFFFFF] flex items-center p-1 mr-5 w-[47px] h-[23px] box-sizing-border cursor-pointer transition duration-300 hover:shadow-[0px_0px_14px_0px_rgba(0,0,0,0.3)]"
+              className="shadow-[0px_0px_14px_0px_rgba(0,0,0,0.15)] rounded-[20px] bg-[#FFFFFF] flex items-center p-1 mr-5 w-[30px] h-[23px] box-sizing-border cursor-pointer transition duration-300 hover:shadow-[0px_0px_14px_0px_rgba(0,0,0,0.3)]"
               onClick={toggleTheme}
             >
               <div
-                className={`rounded-[20px] ${theme === 'light' ? 'bg-[#25293F]' : 'bg-[#FFFFFF]'} w-[19px] h-[19px] ml-auto mr-1 transition duration-300`}
+                className={`rounded-[20px] ${
+                  theme === "light" ? "bg-[#3eb43edd]" : "bg-[#3e57fa]"
+                } w-[19px] h-[19px] ml-auto mr-1 transition duration-300`}
               />
             </div>
-            
-            <div className="rounded-[21px] bg-[url('assets/images/Ellipse139.jpeg')] bg-cover bg-no-repeat w-[42px] h-[42px] transition duration-300" />
+
+            <div
+              className="rounded-[50px] bg-[#c578d2] relative  p-2 w-full text-center cursor-pointer transition duration-300 hover:bg-[#555] font-['Poppins'] font-medium text-[13px] "
+              onClick={() => navigate("/login")}
+            >
+              Login{" "}
+            </div>
+
           </div>
         </div>
         <div className="flex-grow" />
         <div className="absolute bottom-4 right-4">
           <button
-            className="bg-[#414084] text-white p-4 rounded-full shadow-lg transition duration-300 hover:bg-[#555]"
-            onClick={() => navigate('/message')}
+            className="bg-[#ffffff] size-12 rounded-full shadow-lg transition duration-300 hover:bg-[#555]"
+            onClick={() => navigate("/message")}
           >
-            <span className="material-icons">message</span>
+            <span className="material-icons">
+              <img
+                src="https://img.icons8.com/?size=100&id=FpVS74LDFYc2&format=png&color=000000"
+                alt=""
+              />
+            </span>
           </button>
         </div>
       </div>
